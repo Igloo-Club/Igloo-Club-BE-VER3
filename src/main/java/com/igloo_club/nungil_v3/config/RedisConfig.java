065927c81e13c4928694d6e26ca27f761d36e7d6
clap-0 <clap-0@naver.com>
2024-08-05 15:23:17 +0900

    feat: redis 설정 추가

---
+++ src/main/java/com/igloo_club/nungil_v3/config/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.igloo_club.nungil_v3.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisConfigProperties properties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
+        configuration.setPassword(properties.getPassword());
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Getter
+    @ConfigurationProperties(prefix = "spring.redis")
+    @ConstructorBinding
+    @RequiredArgsConstructor
+    private static class RedisConfigProperties {
+
+        private final String host;
+
+        private final int port;
+
+        private final String password;
+    }
+}
