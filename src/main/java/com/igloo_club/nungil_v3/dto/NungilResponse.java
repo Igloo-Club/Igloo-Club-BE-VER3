623db1e98480b54103fe26ae750aa5f533356c8c
ryuseunghan <jorzor99@gmail.com>
2024-10-07 11:19:19 +0900

    feat : 눈길 뽑기를 위한 NungilResponse, MemberMapper

---
+++ src/main/java/com/igloo_club/nungil_v3/dto/NungilResponse.java
@@ -0,0 +1,33 @@
+package com.igloo_club.nungil_v3.dto;
+
+import com.igloo_club.nungil_v3.domain.enums.MbtiType;
+import com.igloo_club.nungil_v3.domain.enums.Religion;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class NungilResponse {
+    private Long id;
+
+    private String nickname;
+
+    private LocalDate birthdate;
+
+    private String companyName;
+
+    private Integer height;
+
+    private Religion religion;
+
+    private Boolean tattoo;
+
+    private Boolean smoke;
+
+    private Integer marriagePlan;
+
+    private MbtiType mbtiType;
+}
