0d247934ac7bd4f08c76e7af98d3d4ec62d243b6
ryuseunghan <jorzor99@gmail.com>
2024-12-08 18:34:47 +0900

    fix : 눈길 맵퍼 오류 수정 member -> opponent

--- src/main/java/com/igloo_club/nungil_v3/mapper/NungilMapper.java
+++ src/main/java/com/igloo_club/nungil_v3/mapper/NungilMapper.java
@@ -11,16 +11,16 @@ public interface NungilMapper {
     NungilMapper INSTANCE = Mappers.getMapper(NungilMapper.class);
 
     // Nungil 엔티티에서 NungilResponse로 매핑
-    @Mapping(source = "nungil.member.id", target = "id")
-    @Mapping(source = "nungil.member.nickname", target = "nickname")
-    @Mapping(source = "nungil.member.birthdate", target = "birthdate")
-    @Mapping(source = "nungil.member.company.companyName", target = "companyName")
-    @Mapping(source = "nungil.member.profile.height", target = "height")
-    @Mapping(source = "nungil.member.profile.religion", target = "religion")
-    @Mapping(source = "nungil.member.profile.tattoo", target = "tattoo")
-    @Mapping(source = "nungil.member.profile.smoke", target = "smoke")
-    @Mapping(source = "nungil.member.profile.marriagePlan", target = "marriagePlan")
-    @Mapping(source = "nungil.member.profile.mbtiType", target = "mbtiType")
+    @Mapping(source = "nungil.opponent.id", target = "id")
+    @Mapping(source = "nungil.opponent.nickname", target = "nickname")
+    @Mapping(source = "nungil.opponent.birthdate", target = "birthdate")
+    @Mapping(source = "nungil.opponent.company.companyName", target = "companyName")
+    @Mapping(source = "nungil.opponent.profile.height", target = "height")
+    @Mapping(source = "nungil.opponent.profile.religion", target = "religion")
+    @Mapping(source = "nungil.opponent.profile.tattoo", target = "tattoo")
+    @Mapping(source = "nungil.opponent.profile.smoke", target = "smoke")
+    @Mapping(source = "nungil.opponent.profile.marriagePlan", target = "marriagePlan")
+    @Mapping(source = "nungil.opponent.profile.mbtiType", target = "mbtiType")
     NungilResponse toResponse(Nungil nungil);
 
 }
